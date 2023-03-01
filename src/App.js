2db39a6caf6d18c3cd8304fe81b6ac51c3529d55
igorpsfreitas <igorpablo.sf@gmail.com>
2023-03-01 08:45:27 -0400

    coming add

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App teste.
+          App teste (coming soon....)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
